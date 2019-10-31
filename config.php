96d964d063e053887c18d28ea1401012dc017256
erebeyrolle <rickymegane3rs@gmail.com>
2019-10-31 14:13:59 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+define ('TITLE', 'Accueil Site Web PHP');// TConstante titre page accueil
+define ('TITLE1', 'Formulaire');// Constante titre page formulaire
+define ('TITLE2', 'Participation');// Constante titre page formulaireparticipation
